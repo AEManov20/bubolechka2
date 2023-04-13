0e33db77bd0322cda204650fad9b9f04ee78524a
AEManov20 <AEManov20@codingburgas.bg>
2023-04-13 13:44:13 +0300

    Add Italian and Finnish to the translations

--- lib/data/categories.dart
+++ lib/data/categories.dart
@@ -4,25 +4,57 @@ import '../models/bubo_category.dart';
 /// This file provides the original category data
 ///
 
-/// Original labels in german and bulgarian, english added as last value
+/// Original labels in german, bulgarian, english and french added as last value
 const labels = [
-  ["1", "Obst", "плодове", "vegetables"],
-  ["2", "Farben", "цветове", "colors"],
-  ["3", "Moebel", "мебели", "furniture"],
-  ["4", "Sachen", "дрехи", "clothes"],
-  ["5", "Spielzeug", "играчки", "toys"],
-  ["6", "Tiere", "животни", "animals"],
-  ["7", "Wilde Tiere", "диви животни", "wild animals"],
-  ["8", "Zahlen", "цифри", "digits"],
-  ["9", "Wetter", "времето", "weather"],
-  ["10", "Zu Hause", "вкъщи", "at home"],
-  ["11", "Natur", "природа", "nature"],
-  ["12", "Im Meer", "морето", "sea"],
-  ["13", "Am Strand", "на плажа", "on the beach"],
-  ["14", "Sport", "спорт", "sport"],
-  ["15", "Stadt", "град", "city"],
-  ["16", "Berufe", "професии", "professions"],
-  ["17", "Fahrzeuge", "транспрорт", "transport"]
+  ["1", "Obst", "плодове", "vegetables", "légumes", "verdure", "vihannekset"],
+  ["2", "Farben", "цветове", "colors", "couleurs", "colori", "värit"],
+  ["3", "Moebel", "мебели", "furniture", "meubles", "mobili", "huonekalut"],
+  ["4", "Sachen", "дрехи", "clothes", "vêtements", "vestiti", "vaatteet"],
+  ["5", "Spielzeug", "играчки", "toys", "jouets", "giocattoli", "lelut"],
+  ["6", "Tiere", "животни", "animals", "animaux", "animali", "eläimet"],
+  [
+    "7",
+    "Wilde Tiere",
+    "диви животни",
+    "wild animals",
+    "animaux sauvages",
+    "animali selvatici",
+    "villieläimet"
+  ],
+  ["8", "Zahlen", "цифри", "digits", "chiffres", "cifre", "numerot"],
+  ["9", "Wetter", "времето", "weather", "le temps", "tempo", "sää"],
+  ["10", "Zu Hause", "вкъщи", "at home", "à la maison", "a casa", "kotona"],
+  ["11", "Natur", "природа", "nature", "nature", "natura", "luonto"],
+  ["12", "Im Meer", "морето", "sea", "mer", "mare", "meri"],
+  [
+    "13",
+    "Am Strand",
+    "на плажа",
+    "on the beach",
+    "sur la plage",
+    "in spiaggia",
+    "rannalla"
+  ],
+  ["14", "Sport", "спорт", "sport", "sport", "sport", "urheilu"],
+  ["15", "Stadt", "град", "city", "ville", "città", "kaupunki"],
+  [
+    "16",
+    "Berufe",
+    "професии",
+    "professions",
+    "métiers",
+    "professioni",
+    "ammatit"
+  ],
+  [
+    "17",
+    "Fahrzeuge",
+    "транспрорт",
+    "transport",
+    "transport",
+    "trasporto",
+    "liikenne"
+  ],
 ];
 
 ///
@@ -37,6 +69,9 @@ List<BuboCategory> buboCategories = labels.map((categoryLabels) {
       {
         'us': categoryLabels[3],
         'de': categoryLabels[1],
-        'bg': categoryLabels[2]
+        'bg': categoryLabels[2],
+        'fr': categoryLabels[4],
+        'it': categoryLabels[5],
+        'fi': categoryLabels[6]
       });
 }).toList();
